054211010053551450fd51a4defe07a7f75788df
Trevor muchel <trevormichel8@gmail.com>
2026-02-02 23:48:20 -0700

    Change MobileShell import to named import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { ReactNode } from "react";
-import MobileShell from "./components/MobileShell";
+import { MobileShell } from "./components/MobileShell";
 
 export const metadata = {
   title: "Aiexor",
